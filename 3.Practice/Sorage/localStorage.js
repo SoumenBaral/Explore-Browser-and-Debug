7c09126de5993756ca363f737d86cad4a5c8874d
SoumenBaral <shumen.sb@gmail.com>
2023-07-15 18:37:05 +0600

    local storage level 1 done

--- 3.Practice/Sorage/localStorage.js
+++ 3.Practice/Sorage/localStorage.js
@@ -4,9 +4,21 @@ const addShakib = ()=>{
    
 
 }
-const Name = localStorage.getItem("name");
+const Names = localStorage.getItem("name");
 const age = localStorage.getItem('age');
  x = document.getElementById('name');
-x.innerText = `${Name} 
+x.innerText = `${Names} 
 Age: ${age}`;
-console.log(Name);
+console.log(Names);
+
+const saveName = localStorage.getItem("Full_Name");
+let Name = {};
+if(saveName){
+    Name = JSON.parse(saveName);
+}
+
+Name['fistName ' ] ="soumen";
+Name['SecondName ' ] ="Baral";
+
+const CartStringify = JSON.stringify(Name);
+localStorage.setItem('Full_Name', CartStringify);
