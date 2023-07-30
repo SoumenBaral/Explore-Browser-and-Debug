c9d3ff6a01b4369b7d4ad3d95cae50728e78df42
SoumenBaral <shumen.sb@gmail.com>
2023-07-31 01:28:24 +0600

    all done

---
+++ 8.javaScript that I need to start React/1.Fundamental.js
@@ -0,0 +1,12 @@
+//1.variable 
+//2. loop 
+//3. function
+//4. array
+//5. Object
+//6.Condition
+// Thats the 6th main fundamental of javascript ...
+
+//Es6
+
+// 1. Template String ...``
+// 2. Arrow Function ...
