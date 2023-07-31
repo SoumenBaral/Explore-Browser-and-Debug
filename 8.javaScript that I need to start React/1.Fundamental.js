18a09e6a6550232b1a78ce3204fde59ceb0c7939
SoumenBaral <shumen.sb@gmail.com>
2023-07-31 10:19:19 +0600

    js motamuti ses

--- 8.javaScript that I need to start React/1.Fundamental.js
+++ 8.javaScript that I need to start React/1.Fundamental.js
@@ -1,7 +1,7 @@
 //1.variable 
 //2. loop 
 //3. function
-//4. array
+//4. array declare , index, length , index of , includes, push ,pop.
 //5. Object
 //6.Condition
 // Thats the 6th main fundamental of javascript ...
@@ -10,3 +10,11 @@
 
 // 1. Template String ...``
 // 2. Arrow Function ...
+// 3. Spread Operator 
+// 4. Array Method  map, find , filter;
+// 5. Destructuring of an array..
+// 6. for of into the array 
+// 7. for in is for Object ...
+// 8. Ternary Operator 
+
+// local Storage and session Storage ............
