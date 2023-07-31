f65cbf62035e658c8468b27c4dcccf1c0fce5b6a
SoumenBaral <shumen.sb@gmail.com>
2023-07-31 14:35:16 +0600

    coursel is done

---
+++ 91.my coursel/js/slider.js
@@ -0,0 +1,23 @@
+const images = [
+    'images/pic-1.jpg', 
+    'images/pic-2.jpg', 
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg',
+]
+
+let imgIndex = 0;
+const imageElement = document.getElementById('image_slider')
+setInterval(()=>{
+    if (imgIndex===images.length) {
+        imgIndex=0
+    }
+    const imgUrl = images[imgIndex];
+    // console.log(imgUrl);
+    imageElement.setAttribute('src',imgUrl);
+    imgIndex++;
+},1000)
